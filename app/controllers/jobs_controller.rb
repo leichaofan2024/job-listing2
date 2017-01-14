2b9d1e35337a03e93f7c4eb5c4fc98954d10853d
leichaofan <leichaofan2024@gmail.com>
2017-01-14 13:08:17 +0800

    part 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,10 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "你没有访问权限！"
+      redirect_to root_path
+    end
   end
   def index
     flash[:notice] = "欢迎来到职缺的世界，总有一个适合你！"
